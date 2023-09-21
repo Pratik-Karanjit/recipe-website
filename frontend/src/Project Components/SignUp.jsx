b7503fbd4f8e9c8f42bfa3b37a73e23c8432062d
Siddhanta Shrestha <sidmarkys2004@gmail.com>
2023-09-21 19:26:06 +0545

    push

--- frontend/src/Project Components/SignUp.jsx
+++ frontend/src/Project Components/SignUp.jsx
@@ -1,57 +1,100 @@
 import React, { useState } from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import { Modal, Button, Form, Nav } from "react-bootstrap";
 
 function SignUp() {
   const [show, setShow] = useState(false);
+  const [isSignup, setIsSignup] = useState(true); // To track whether the signup or login section is active
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+    setIsSignup(true); // Reset to the signup section when the modal is closed
+  };
+
+  const handleShow = () => {
+    setShow(true);
+    setIsSignup(true); // Show the signup section by default when the modal is opened
+  };
+
+  const toggleSection = () => {
+    setIsSignup(!isSignup); // Toggle between signup and login sections
+  };
 
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
-        Sign Up
+        Sign Up / Login
       </Button>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Sign Up</Modal.Title>
+          <Modal.Title>
+            {isSignup ? "Sign Up" : "Login"}
+            <Button
+              variant="link"
+              className="switch-button"
+              onClick={toggleSection}
+            >
+              {isSignup ? "Switch to Login" : "Switch to Sign Up"}
+            </Button>
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name" />
-            </Form.Group>
+          {isSignup ? (
+            <Form>
+              {/* Signup Form */}
+              <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter your name" />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter your email" />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter your email" />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter your password" />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter your password"
+                />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPasswordConfirm">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Confirm your password"
-              />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPasswordConfirm">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm your password"
+                />
+              </Form.Group>
 
-            <Button variant="primary" type="submit">
-              Sign Up
-            </Button>
-          </Form>
+              <Button variant="primary" type="submit">
+                Sign Up
+              </Button>
+            </Form>
+          ) : (
+            <Form>
+              {/* Login Form */}
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter your email" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter your password"
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            </Form>
+          )}
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
+        <Modal.Footer>{/* Empty footer */}</Modal.Footer>
       </Modal>
     </>
   );
